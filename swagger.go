7d2a7b304bddc5aec38c3c54203332709ee4a029
dub-squared <>
2020-11-11 11:30:35 -0500

    removed unneeded file

--- swagger.go
+++
@@ -1,4 +0,0 @@
-package swagger
-
-// Swagger requires a go file in the root directory in order to run properly, so this is that file
-// To generate docs run `swag init -g ./cmd/rest-api/main.go -o ./docs`
